2d7c31050c1c57de0e3e630056bcc6d314f51307
Guilherme Afonso <guilherme.argomes@gmail.com>
2019-05-14 16:39:57 -0300

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import util.PCV;
+import util.Leitor;
+/**
+ * @author Guilherme Afonso R. Gomes
+ * Inteligencia Computacional para otimização
+ * Problema do Caixeiro Viajante
+ */
+public class main {
+
+
+    public static void main(String[] args) throws IOException {
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.setVisible(true);
+
+
+       
+    }
+    
+}
